232edb86f07b66417705129eef12ca3feeb6e39f
gregoirebechade <gregoire.bechade@gmail.com>
2024-12-05 12:32:26 +0100

    h

--- src/train.py
+++ src/train.py
@@ -143,8 +143,8 @@ class QNetwork(nn.Module): # approxime Q(s,a); A un état s, on associe Q(s,a) p
         self.fc3 = nn.Linear(36, output_dim)
 
     def forward(self, x):
-        x = torch.Relu(self.fc1(x))
-        x = torch.Relu(self.fc2(x))
+        x = torch.ReLU(self.fc1(x))
+        x = torch.ReLU(self.fc2(x))
         return self.fc3(x)
 
 # Replay Buffer for Experience Replay
